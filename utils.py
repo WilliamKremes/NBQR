53448446ddfc559480cc01f5e26f60d5900e3653
WilliamKremes <williamkremes2013@gmail.com>
2025-08-18 16:21:28 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+# utils/geodesy.py
 from pyproj import Geod
 
 geod = Geod(ellps="WGS84")
@@ -6,9 +7,17 @@ def meters_to_latlon_distances(meters, lat, azimuth=0):
     """
     Calcula deslocamento em graus de latitude e longitude a partir
     de um deslocamento em metros e azimute em graus.
+
+    Parâmetros:
+        meters (float): distância em metros
+        lat (float): latitude de referência em graus
+        azimuth (float): direção do deslocamento em graus (0=norte, 90=leste)
+    
+    Retorna:
+        tuple: (delta_lat, delta_lon) em graus
     """
-    # Azimuth 0 = norte, 90 = leste
-    lon0, lat0, _ = 0, lat, 0  # lat base, lon base irrelevante para delta
+    # Ponto inicial fictício (lon0 irrelevante)
+    lon0, lat0 = 0, lat
     lon1, lat1, _ = geod.fwd(lon0, lat0, azimuth, meters)
     delta_lat = lat1 - lat0
     delta_lon = lon1 - lon0
