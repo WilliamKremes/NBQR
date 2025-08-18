342ab72a7b7b51a081427f2dd6ac175999e5c575
WilliamKremes <williamkremes2013@gmail.com>
2025-08-18 16:15:59 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
-from math import cos, radians
+from pyproj import Geod
 
-def meters_to_latlon_distances(meters, lat):
-    """Converte deslocamento em metros para graus de latitude e longitude
-    considerando a latitude atual (em graus)."""
-    delta_lat = meters / 111320.0  # metros para graus latitude
-    delta_lon = meters / (40075000.0 * cos(radians(lat)) / 360.0)  # metros para graus longitude
+geod = Geod(ellps="WGS84")
+
+def meters_to_latlon_distances(meters, lat, azimuth=0):
+    """
+    Calcula deslocamento em graus de latitude e longitude a partir
+    de um deslocamento em metros e azimute em graus.
+    """
+    # Azimuth 0 = norte, 90 = leste
+    lon0, lat0, _ = 0, lat, 0  # lat base, lon base irrelevante para delta
+    lon1, lat1, _ = geod.fwd(lon0, lat0, azimuth, meters)
+    delta_lat = lat1 - lat0
+    delta_lon = lon1 - lon0
     return delta_lat, delta_lon
