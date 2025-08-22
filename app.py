3c4ff07534020cb1a23943d227402a79666ae435
WilliamKremes <williamkremes2013@gmail.com>
2025-08-22 00:22:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,9 @@ def index():
         estabilidade=estabilidade
     )
 
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", "10000"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)  # <--- debug ativado
+
