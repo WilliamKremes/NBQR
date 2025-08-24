aa94f5c6305b4d69b7fb72e14aedc994e82ffa33
WilliamKremes <williamkremes2013@gmail.com>
2025-08-23 22:54:53 -0300

    Update agentes_quimicos.py

--- agentes_quimicos.py
+++ agentes_quimicos.py
@@ -1,10 +1,37 @@
 agentes_nao_persistentes = {
-    "sarin", "tabun", "cloro", "fosgênio", "cianeto de hidrogênio",
-    "cloreto de cianogênio", "cs", "cn", "amônia", "gás natural",
-    "cloreto de hidrogênio", "formaldeído", "dióxido de enxofre"
+    # Nervosos (G)
+    "sarin", "tabun", "soman", "gf",
+    
+    # Asfixiantes
+    "cloro", "fosgênio", "difosgênio", "óxidos de nitrogênio", 
+    "scl2", "bf3",
+    
+    # Sangue
+    "cianeto de hidrogênio", "cloreto de cianogênio", "arsina", "monóxido de carbono",
+    
+    # Lacrimogênios
+    "cs", "cn", "cr", "bromoacetona", "ca", "cba",
+    
+    # TICs voláteis
+    "amônia", "gás natural", "cloreto de hidrogênio", 
+    "formaldeído", "dióxido de enxofre", "brometo de metila"
 }
 
 agentes_persistentes = {
-    "vx", "vr", "mostarda", "hd", "mostarda nitrogenada", "hn",
-    "lewisita", "l", "adamsita", "dm", "mercúrio", "tricloroetileno"
+    # Nervosos (V)
+    "vx", "vr", "ve", "vm", "vg",
+    
+    # Vesicantes
+    "mostarda", "hd", "t", "hl", "q", 
+    "mostarda nitrogenada", "hn1", "hn2", "hn3",
+    
+    # Arsenicais
+    "lewisita", "l", "sa",
+    
+    # Incapacitantes persistentes
+    "adamsita", "dm", "da",
+    
+    # Industriais persistentes
+    "mercúrio", "tricloroetileno", "tetracloreto de carbono", 
+    "benzeno", "dioxinas", "pcbs"
 }
